f3db0302dc7dffaf554090f3423814e651a56ceb
Smubge <87445504+Smubge@users.noreply.github.com>
2025-09-09 15:14:48 -0400

    Added cfg python
    - Added Control Flow Graphs
    - Added Block Functionality

---
+++ cfg.py
@@ -0,0 +1,98 @@
+import json
+
+class Block:
+	def __init__(self, idx, instrs):
+		self.idx = idx
+		self.instrs = instrs
+		self.edges = []
+
+	def label(self):
+		if self.instrs and "labels" in self.instrs[0]:
+			return self.instrs[0]["labels"]
+		return None
+	
+	def last(self):
+		if self.instrs:
+			return self.instrs[-1]
+		return None
+
+	def add_edge(self, target):
+		if target not in self.edges:
+			self.edges.append(target)
+	
+	def __str__(self):
+		return f"Block(idx={self.idx}, label={self.label}, edges = {self.edges} )"          
+
+with open('add.json', 'r') as file:
+	instrs = json.load(file)
+
+blocks = []
+block = []
+lbl2block = {}
+selfIdx = 0
+
+# creating basic blocks implementation
+for func in instrs["functions"]:
+    if "instrs" in func:
+        for instr in func["instrs"]:  # loop over instructions in the function
+            if "label" in instr:
+                b0 = Block("v" + str(selfIdx), block)
+                selfIdx += 1
+                blocks.append(b0)
+                block = []
+                block.append(instr)
+                lbl2block.append(instr["label"])
+            elif "op" in instr:
+                if instr["op"] == "br" or instr["op"] == "jmp":
+                    b1 = Block("v" + str(selfIdx), block)
+                    selfIdx += 1
+                    blocks.append(b1)
+                    block = []
+                    block.append(instr)
+                else:
+                    selfIdx += 1
+                    block.append(instr)
+if block:
+    b2 = Block("v" + str(selfIdx), block)
+    selfIdx += 1
+    blocks.append(b2)
+    block = []
+
+cfg = {}
+# cfg implementation
+"""
+basic blocks:
+* ends with a terminator
+* starts with a label
+* has neither
+* has both
+"""
+
+'''
+for b in blocks:
+	last = b[-1] // last instr in block, doesn't work if blocks empty
+	if last is "jmp":
+		cfg[b.name] = [last.dest] // assume each block has label/id
+	else if last is "br":
+		cfg[b.name] = [last.true_label, last.false_label]
+	else:
+		cfg[b.name] = [(b + 1).name] // next block in the list
+'''
+for b in blocks:
+	last = b.last() # last instr in block, doesn't work if blocks empty
+	if b.last() is not None:
+		if b.last()["op"] == "jmp":
+			cfg[b.idx] = [b.last()["dest"]]
+		elif b.last()["op"] == "br":
+			cfg[b.idx] = [b.last()["labels"][0], b.last()["labels"][1]]
+		elif "dest" not in b.last() and "labels" not in b.last():
+			cfg[b.idx] = []
+		else:
+			text = b.idx[0]
+			num = b.idx[1]
+			new_num = str(int(num) + 1)
+			new_idx = text + new_num
+			cfg[b.idx] = [new_idx]
+
+print(cfg)
+		
